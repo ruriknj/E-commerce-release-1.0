e2875b5559d7072efaf3256310dd2da2db14b30d
ruriknj <ruriknj@hotmail.com>
2022-03-05 22:53:44 -0300

    E-commerce release-1.0

---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,28 @@
+import { Product } from './../../common/product';
+import { ProductService } from './../../services/product.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list-table.component.html',
+ // templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  products: Product[] = [];
+
+  constructor(private productService:ProductService) { }
+
+  ngOnInit() {
+    this.listProducts();
+  }
+
+  listProducts() {
+    this.productService.getProductList().subscribe(
+      data => {
+        this.products = data;
+      }
+    )
+  }
+}
